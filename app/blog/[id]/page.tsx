8b4163d9e88cc2c2ef025b26a406a8d34b495cc6
Егор Шерстнёв <egorserstnev@MacBook-Pro-Egor.local>
2025-03-11 22:32:21 +0300

    added metadata

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,7 +1,17 @@
+import { Metadata } from "next"
+
 type Props = {
     params: {
         id: string,
     }
+}
+
+export async function generateMetadata ({
+    params: { id }
+}: Props): Promise<Metadata> {
+   return {
+      title: `Post ${id} | Next App`,
+   }
 }
 
 export default function Post({params: { id }}: Props) {
