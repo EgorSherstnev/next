34c5a6475c8431dc3c902385a25cfa03d0355c1b
Егор Шерстнёв <egorserstnev@MacBook-Pro-Egor.local>
2025-03-29 17:10:20 +0300

    added loading and error pages

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,5 +1,15 @@
 import { Metadata } from "next"
 
+async function getData(id: string) {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+        next: {
+            revalidate: 60
+        }
+    });
+
+    return response.json()
+}
+
 type Props = {
     params: {
         id: string,
@@ -9,11 +19,21 @@ type Props = {
 export async function generateMetadata ({
     params: { id }
 }: Props): Promise<Metadata> {
-   return {
-      title: `Post ${id} | Next App`,
-   }
+    const post = await getData(id)
+    console.log('post in generateMetadata: ', post)
+
+    return {
+        title: post.title,
+    }
 }
 
-export default function Post({params: { id }}: Props) {
-   return <h1>Post page {id}</h1>
+export default async function Post({params: { id }}: Props) {
+    const post = await getData(id)
+
+    return (
+        <>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+        </>
+    )
 }
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,9 +1,41 @@
 import { Metadata } from "next"
+import Link from "next/link"
+
+async function getData() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+        // const response = await fetch('https://jsonplaceholder.typicode.com/postssdfgsdggd', {
+
+        next: {
+            revalidate: 60
+        }
+    });
+
+    // if (!response.ok) {
+    //     throw new Error(`HTTP error! status: ${response.status}`)
+    // }
+
+    return response.json()
+}
 
 export const metadata: Metadata = {
   title: 'Blog | Next App',
 }
 
-export default function Blog() {
-   return <h1>Blog page</h1>
+export default async function Blog() {
+    const posts = await getData()
+
+    return (
+        <>
+            <h1>Blog page</h1>
+            <ul>
+                {posts.map((post: any) => (
+                    <li key={post.id}>
+                        <Link href={`/blog/${post.id}`}>
+                            {post.title}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </>
+    )
 }
