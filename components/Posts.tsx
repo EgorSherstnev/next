ef7005c91add291ffb319bb348ff8a9be2a705e9
Егор Шерстнёв <egorserstnev@MacBook-Pro.local>
2025-06-04 23:09:32 +0300

    updated styles

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -3,23 +3,21 @@
 import { usePosts } from '@/store'
 import Link from 'next/link'
 import React, { useEffect } from 'react'
+import { shallow } from 'zustand/shallow'
 
-// type Props = {
-//     posts: any[]
-// }
 const Posts = () => {
-    const [ posts, loading, getAllPosts ] = usePosts((state) => ([
-        state.posts,
-        state.loading,
-        state.getAllPosts
-        ]
-    ))
+    const { posts, loading } = usePosts(state => ({
+        posts: state.posts,
+        loading: state.loading
+    }), shallow)
 
     useEffect(() => {
-        getAllPosts()
-    }, [getAllPosts])
-    return (loading ? ( <h3>Loading</h3> 
-        ) : (     
+        usePosts.getState().getAllPosts()
+    }, [])
+
+    return loading ? (
+        <h3>Loading</h3>
+    ) : (
         <ul>
             {posts.map((post: any) => (
                 <li key={post.id}>
@@ -28,9 +26,8 @@ const Posts = () => {
                     </Link>
                 </li>
             ))}
-        </ul> 
-        )
+        </ul>
     )
 }
 
-export {Posts}
+export { Posts }
