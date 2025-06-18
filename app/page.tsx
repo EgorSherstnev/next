c2725bcc35a72ba406d4c1305729f832d4008e3d
Егор Шерстнёв <egorserstnev@MacBook-Pro.local>
2025-06-18 09:26:40 +0300

    added google signin

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
-
+import SignInButton from './SignInButton';
 
 export default function Home() {
   return (
-    <h1>NextJS</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="mb-4 text-2xl font-bold">NextJS</h1>
+      <SignInButton />
+    </div>
   )
 }
